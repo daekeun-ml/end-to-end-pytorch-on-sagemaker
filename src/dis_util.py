bfc066c8de57d52a972a4d9bebdc27395e11e3cd
EC2 Default User <ec2-user@ip-172-16-37-23.us-east-2.compute.internal>
2020-08-04 13:10:53 +0000

    update

---
+++ src/dis_util.py
@@ -0,0 +1,149 @@
+
+import argparse
+import logging
+import numpy as np
+import os
+import random
+import sys
+import warnings
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data.distributed
+
+import sagemaker_containers
+
+try:
+    from apex.parallel import DistributedDataParallel as DDP
+    from apex.fp16_utils import *
+    from apex import amp, optimizers
+    from apex.multi_tensor_apply import multi_tensor_applier
+except ImportError:
+    raise ImportError(
+        "Please install apex from https://www.github.com/nvidia/apex to run this example.")
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def dist_init(fn, args):
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
+
+    args.is_distributed = len(args.hosts) > 1 and args.backend is not None
+    args.is_multigpus = args.num_gpus > 1
+    args.multigpus_distributed = (args.is_distributed or args.is_multigpus)
+
+    logger.debug("Distributed training - {}".format(args.is_distributed))
+    logger.debug("Number of gpus available - {}".format(args.num_gpus))
+
+    args.world_size = 1
+    if args.multigpus_distributed:
+        # Initialize the distributed environment.
+        args.apex = True
+        args.world_size = len(args.hosts) * args.num_gpus
+        os.environ['WORLD_SIZE'] = str(args.world_size)
+        args.host_num = args.hosts.index(args.current_host)
+        mp.spawn(fn, nprocs=args.num_gpus, args=(args,))
+    else:
+        current_gpu = 0
+        fn(current_gpu, args)
+
+
+def dist_setting(current_gpu, model, loss_fn, args):
+    print("channels_last : {}".format(args.channels_last))
+    if args.channels_last:
+        args.memory_format = torch.channels_last
+    else:
+        args.memory_format = torch.contiguous_format
+
+    if args.apex:
+        args.lr = args.lr*float(args.batch_size*args.world_size)/256.
+    args.current_gpu = current_gpu
+    if args.current_gpu is not None:
+        print("Use GPU: {} for training".format(args.current_gpu))
+
+    if args.multigpus_distributed:
+        args.rank = args.num_gpus * args.host_num + args.current_gpu
+        dist.init_process_group(backend=args.backend,
+                                rank=args.rank, world_size=args.world_size)
+        logger.info('Initialized the distributed environment: \'{}\' backend on {} nodes. '.format(
+            args.backend, dist.get_world_size()) + 'Current host rank is {}. Number of gpus: {}'.format(
+            dist.get_rank(), args.num_gpus))
+
+    if args.sync_bn:
+        import apex
+        print("using apex synced BN")
+        model = apex.parallel.convert_syncbn_model(model)
+
+    if args.multigpus_distributed:
+        if args.current_gpu is not None:
+            torch.cuda.set_device(args.current_gpu)
+            args.batch_size = int(args.batch_size / args.num_gpus)
+            if not args.apex:
+                model.cuda(args.current_gpu)
+                model = torch.nn.parallel.DistributedDataParallel(
+                    model, device_ids=[args.current_gpu])
+        else:
+            if not args.apex:
+                model.cuda()
+                model = torch.nn.parallel.DistributedDataParallel(model)
+    elif args.current_gpu is not None:
+        torch.cuda.set_device(args.current_gpu)
+        if not args.apex:
+            model = model.cuda(args.current_gpu)
+    else:
+        if not args.apex:
+            model = torch.nn.DataParallel(model).cuda()
+
+    return model, args
+
+
+def apex_init(model, optimizer, args):
+    model = model.cuda().to(memory_format=args.memory_format)
+    model, optimizer = amp.initialize(model, optimizer,
+                                      opt_level=args.opt_level,
+                                      keep_batchnorm_fp32=args.keep_batchnorm_fp32,
+                                      loss_scale=args.loss_scale
+                                      )
+    if args.multigpus_distributed:
+        model = DDP(model, delay_allreduce=True)
+    return model, optimizer
+
+
+def reduce_tensor(tensor, args):
+    rt = tensor.clone()
+    dist.all_reduce(rt, op=dist.reduce_op.SUM)
+    rt /= args.world_size
+    return rt
+
+
+def fast_collate(batch, memory_format=torch.channels_last):
+    imgs = [img[0] for img in batch]
+    targets = torch.tensor([target[1] for target in batch], dtype=torch.int64)
+    w = imgs[0].size[0]
+    h = imgs[0].size[1]
+    tensor = torch.zeros((len(imgs), 3, h, w), dtype=torch.uint8).contiguous(
+        memory_format=memory_format)
+    for i, img in enumerate(imgs):
+        nump_array = np.array(img, dtype=np.uint8)
+        if(nump_array.ndim < 3):
+            nump_array = np.expand_dims(nump_array, axis=-1)
+        nump_array = np.rollaxis(nump_array, 2)
+        tensor[i] += torch.from_numpy(nump_array)
+    return tensor, targets
---
+++ src/util.py
@@ -0,0 +1,202 @@
+
+import codecs
+import json
+import logging
+import os
+import shutil
+import sys
+import time
+
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data.distributed
+from torchvision import models
+
+import sagemaker_containers
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def torch_model(model_name, pretrained=True):
+    model_names = sorted(name for name in models.__dict__
+                         if name.islower() and not name.startswith("__")
+                         and callable(models.__dict__[name]))
+
+    if(model_name == "inception_v3"):
+        raise RuntimeError(
+            "Currently, inception_v3 is not supported by this example.")
+
+    # create model
+    if pretrained:
+        print("=> using pre-trained model '{}'".format(model_name))
+        model = models.__dict__[model_name](pretrained=True)
+    else:
+        print("=> creating model '{}'".format(model_name))
+        model = models.__dict__[model_name]()
+    return model
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def save_model(state, is_best, model_dir):
+    logger.info("Saving the model.")
+    filename = os.path.join(model_dir, 'checkpoint.pth')
+    # recommended way from http://pytorch.org/docs/master/notes/serialization.html
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, os.path.join(model_dir, 'model_best.pth'))
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+        
+def adjust_learning_rate(optimizer, epoch, step, len_epoch, args):
+    """LR schedule that should yield 76% converged accuracy with batch size 256"""
+    factor = epoch // 30
+
+    if epoch >= 80:
+        factor = factor + 1
+
+    lr = args.lr*(0.1**factor)
+
+    """Warmup"""
+    if epoch < 5:
+        lr = lr*float(1 + step + epoch*len_epoch)/(5.*len_epoch)
+
+    if(args.current_gpu == 0):
+        print("epoch = {}, step = {}, lr = {}".format(epoch, step, lr))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+        
+        
+class data_prefetcher():
+    def __init__(self, loader):
+        self.loader = iter(loader)
+        self.stream = torch.cuda.Stream()
+        self.mean = torch.tensor([0.5 * 255, 0.5 * 255, 0.5 * 255]).cuda().view(1,3,1,1)
+        self.std = torch.tensor([0.5 * 255, 0.5 * 255, 0.5 * 255]).cuda().view(1,3,1,1)
+        self.preload()
+
+    def preload(self):
+        try:
+            self.next_input, self.next_target = next(self.loader)
+        except StopIteration:
+            self.next_input = None
+            self.next_target = None
+            return
+
+        with torch.cuda.stream(self.stream):
+            self.next_input = self.next_input.cuda(non_blocking=True)
+            self.next_target = self.next_target.cuda(non_blocking=True)
+            self.next_input = self.next_input.float()
+            self.next_input = self.next_input.sub_(self.mean).div_(self.std)
+
+    def next(self):
+        torch.cuda.current_stream().wait_stream(self.stream)
+        input = self.next_input
+        target = self.next_target
+        if input is not None:
+            input.record_stream(torch.cuda.current_stream())
+        if target is not None:
+            target.record_stream(torch.cuda.current_stream())
+        self.preload()
+        return input, target
+    
+
+
+def save_history(path, history):
+
+    history_for_json = {}
+    # transform float values that aren't json-serializable
+    for key in history.keys():
+        history_for_json[key] = list(map(float, history[key]))
+
+    with codecs.open(path, 'w', encoding='utf-8') as f:
+        json.dump(history_for_json, f, separators=(
+            ',', ':'), sort_keys=True, indent=4)
+        
+        
+
+def init_modelhistory(model_history):
+    model_history['epoch'] = []
+    model_history['batch_idx'] = []
+    model_history['batch_time'] = []
+    model_history['losses'] = []
+    model_history['top1'] = []
+    model_history['top5'] = []
+    model_history['val_epoch'] = []
+    model_history['val_batch_idx'] = []
+    model_history['val_batch_time'] = []
+    model_history['val_losses'] = []
+    model_history['val_top1'] = []
+    model_history['val_top5'] = []
+    model_history['val_avg_epoch'] = []
+    model_history['val_avg_batch_time'] = []
+    model_history['val_avg_losses'] = []
+    model_history['val_avg_top1'] = []
+    model_history['val_avg_top5'] = []
+    return model_history
